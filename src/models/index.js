48e466e370a306545c2fc485e2db0ad135f6d4b5
brandon quezada <brandon.cypaso@gmail.com>
2020-10-26 23:08:03 -0500

    commit again. pre heroku push

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,7 @@ if (process.env.DATABASE_URL) {
 		dialect: 'postgres',
 	});
 } else {
-	const sequelize = new Sequelize(
+	sequelize = new Sequelize(
 		process.env.TEST_DATABASE || process.env.DATABASE,
 		process.env.DATABASE_USER,
 		process.env.DATABASE_PASSWORD,
